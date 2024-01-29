b8e68f85945beb1fa43f0f0e1c21974fcc3e4358
rockclimber147 <daylencsmith@gmail.com>
2024-01-29 15:58:39 -0800

    Implement sliders

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,22 @@ import display.GameWindow;
 import model.GridCell;
 import model.Renderer;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        int edgeCellCount = 500;
-        GridCell.setIterationCount(160);
+        int edgeCellCount = 400;
+        GridCell.setIterationCount(100);
 
         Renderer renderer = new Renderer(edgeCellCount);
         GameWindow window = new GameWindow();
-        window.add(renderer.getDisplay());
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout());
+        mainPanel.add(renderer.getDisplay());
+        mainPanel.add(renderer.getControlPanel());
+        window.add(mainPanel);
+        mainPanel.setPreferredSize(new Dimension(2 * edgeCellCount + 5, edgeCellCount));
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
