a4e2428f6a910773f1ba8d94fbd4ebbdcaef6d86
Daylen Smith <daylencsmith@gmail.com>
2024-01-29 21:36:06 -0800

    Cleanup and document ComplexNumber class

--- src/complexNumbers/ComplexNumber.java
+++ src/complexNumbers/ComplexNumber.java
@@ -1,62 +1,101 @@
 package complexNumbers;
 
+/**
+ * Models a complex number with real part and imaginary part
+ * @author  Daylen Smith
+ * @version 2024
+ */
 public class ComplexNumber {
     private double realPart;
     private double complexPart;
 
-    public ComplexNumber(double realPart, double complexPart){
+    /**
+     * Creates a complex number
+     * @param realPart The real part of the number
+     * @param complexPart The complex part of the number
+     */
+    public ComplexNumber(final double realPart, final double complexPart){
         this.realPart = realPart;
         this.complexPart = complexPart;
     }
 
+    /**
+     * Returns the real part of the complex number
+     * @return The real part
+     */
     public double getRealPart() {
         return realPart;
     }
 
-    public void setRealPart(double realPart) {
+    /**
+     * Sets the real part
+     * @param realPart The new real part
+     */
+    public void setRealPart(final double realPart) {
         this.realPart = realPart;
     }
 
+    /**
+     * Returns the complex part of the complex number
+     * @return The complex part
+     */
     public double getComplexPart() {
         return complexPart;
     }
 
-    public void setComplexPart(double complexPart) {
+    /**
+     * Sets the complex part of the complex number
+     * @param complexPart The new complex part
+     */
+    public void setComplexPart(final double complexPart) {
         this.complexPart = complexPart;
     }
 
-    public void add(ComplexNumber toAdd){
+    /**
+     * Sets this complex number to the sum of itself and another complex number
+     * @param toAdd The complex number to add
+     */
+    public void add(final ComplexNumber toAdd){
         this.realPart += toAdd.realPart;
         this.complexPart += toAdd.complexPart;
     }
 
-    public void multiply(ComplexNumber toMultiply){
+    /**
+     * Sets this complex number to the product of itself and another complex number
+     * @param toMultiply The complex number to multiply with
+     */
+    public void multiply(final ComplexNumber toMultiply){
         double newRealPart = this.realPart* toMultiply.realPart - this.complexPart * toMultiply.complexPart;
         double newComplexPart = this.complexPart * toMultiply.realPart + this.realPart * toMultiply.complexPart;
         this.realPart = newRealPart;
         this.complexPart = newComplexPart;
     }
 
-    public void power(int exponent){
+    /**
+     * Multiplies this complex number with itself the amount of times specified
+     * @param exponent The amount of times to multiply this complex number by itself
+     */
+    public void power(final int exponent){
         ComplexNumber initial = this.copy();
         for (int i = 1; i < exponent; i++) {
             this.multiply(initial);
         }
     }
 
+    /**
+     * Returns a copy of this complex number
+     * @return A copy of this complex number
+     */
     public ComplexNumber copy(){
         return new ComplexNumber(this.realPart, this.complexPart);
     }
 
+    /**
+     * Gets the distance of this complex number from the origin
+     * @return The distance to the origin
+     */
     public double getDistanceFromOrigin(){
         return Math.sqrt(this.realPart * this.realPart + this.getComplexPart() * this.getComplexPart());
-    }
-
-    public static void main(String[] args) {
-        ComplexNumber test = new ComplexNumber(2,3);
-        System.out.println(test);
-        test.power(5);
-        System.out.println(test);
     }
 
     @Override
